f384d1de3809baae8ecfc63e03e08decc99f27b4
Artem_Honcharov <shalenstvo228@gmail.com>
2024-05-05 11:51:18 +0300

    add tests for Main method

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+public class MainTest {
+    @Disabled
+    @Test
+    @Timeout(value = 22)
+    void testMainForTimeout() {
+    }
+}
